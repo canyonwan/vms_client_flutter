e7556e3c175e977e3433016b3fc27dd3f352e387
canyonwan <canyonwan@126.com>
2023-08-23 19:58:29 +0800

    feat: 新增组件

---
+++ lib/app/utils/assets_picker.dart
@@ -0,0 +1,28 @@
+part of utils;
+
+abstract class AssetsPicker {
+  static final ImagePicker _imagePicker = ImagePicker();
+
+  static Future<XFile?> image({
+    required BuildContext context,
+    ImageSource source = ImageSource.gallery,
+    double maxWidth = 512,
+    double maxHeight = 512,
+  }) async {
+    if (!(await Access.photos())) {
+      if (context.mounted) {
+        // SmartDialog.show(builder:  (_) => const LoadingDialog());
+        CustomDialog.showAccess(
+          context: context,
+          content: const Text('Requires access to your photo gallery'),
+        );
+      }
+      return null;
+    }
+    return _imagePicker.pickImage(
+      source: source,
+      maxWidth: maxWidth,
+      maxHeight: maxHeight,
+    );
+  }
+}

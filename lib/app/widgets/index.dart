d6cb799341534597ffe353bbbaa8c308d0bb28e5
canyonwan <canyonwan@126.com>
2023-08-23 19:59:49 +0800

    feat: 新增组件

---
+++ lib/app/widgets/index.dart
@@ -0,0 +1,33 @@
+library widgets;
+
+import 'dart:io';
+import 'dart:math';
+
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:pinput/pinput.dart';
+import 'package:vms_client_flutter/app/utils/index.dart';
+import 'package:vms_client_flutter/theme.dart';
+
+part 'alert.dart';
+part 'app_bar.dart';
+part 'avatar.dart';
+part 'bottom_app_bar.dart';
+part 'bottom_navigation_bar.dart';
+part 'button.dart';
+part 'card.dart';
+part 'cell.dart';
+part 'checkbox.dart';
+part 'dialog.dart';
+part 'empty.dart';
+part 'expansion_tile.dart';
+part 'image.dart';
+part 'input.dart';
+part 'loading.dart';
+part 'switch.dart';
+part 'tab_bar.dart';
+part 'tag.dart';

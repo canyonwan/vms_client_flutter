543cd8e26fd6eeb982a45730e51e9ab834a5b2c6
canyonwan <canyonwan@126.com>
2023-08-24 14:58:34 +0800

    feat: 新增runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:vms_client_flutter/app.dart';
 
+import 'global.dart';
+
 void main() {
-  runApp(const App());
+  Global.init().then((_) => runApp(const App()));
 }

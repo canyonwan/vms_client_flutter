89a0561f63c0e41a7cd74da33b42abd2483373c1
canyonwan <canyonwan@126.com>
2023-08-24 14:59:40 +0800

    feat: 新增api url

--- lib/const/common.dart
+++ lib/const/common.dart
@@ -1,2 +1,5 @@
 const double kBorderRadius = 6;
 const double kPagePadding = 10;
+
+const String kProdApiUrl = 'https://api.canyonwan.top/v1/frontend/';
+const String kDevApiUrl = 'http://127.0.0.1:8080/v1/frontend/';

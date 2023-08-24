52f034d6ac61cdaa37a4f3bf06d23985be6a7e73
canyonwan <canyonwan@126.com>
2023-08-24 14:56:45 +0800

    fix: 修改滚动效果

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
-import 'package:vms_client_flutter/const/colors.dart';
-import 'package:vms_client_flutter/lan/Message.dart';
+import 'package:vms_client_flutter/i18n/index.dart';
+import 'package:vms_client_flutter/store/index.dart';
+import 'package:vms_client_flutter/theme.dart';
 
 import 'app/routes/app_pages.dart';
 
@@ -26,28 +28,65 @@ class _AppState extends State<App> {
           navigatorObservers: [FlutterSmartDialog.observer],
           debugShowCheckedModeBanner: false,
           title: '兽用杂货铺',
-          locale: Get.deviceLocale,
-          translations: Messages(),
-          // supportedLocales: [Locale('en'), Locale('zh', 'CN')],
-          // locale: const Locale("zh", "CN"),
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: Translation.supported,
+          fallbackLocale: Translation.fallback,
+          locale: ConfigStore.to.locale,
+          translations: Translation(),
+          theme: AppTheme.light(),
+          darkTheme: AppTheme.dark(),
+          themeMode: AppTheme.mode,
+          popGesture: true,
+          transitionDuration: const Duration(milliseconds: 450),
           initialRoute: AppPages.INITIAL,
           getPages: AppPages.routes,
-          // builder: EasyLoading.init(),
           builder: (ctx, child) {
             FlutterSmartDialog.init();
             ScreenUtil.init(ctx);
-            // return child!;
-            return Theme(
-              data: ThemeData(
-                primarySwatch: createMaterialColor(kAppColor),
-                scaffoldBackgroundColor: kBgF7Color,
-                brightness: Brightness.light,
-              ),
-              child: child!,
+            return ScrollConfiguration(
+              behavior: NoShadowScrollBehavior(),
+              child: child ?? const Material(),
             );
           },
         );
       },
     );
+  }
+}
+
+class NoShadowScrollBehavior extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
+    switch (getPlatform(context)) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        return child;
+      case TargetPlatform.android:
+        return GlowingOverscrollIndicator(
+          showLeading: false,
+          showTrailing: false,
+          axisDirection: details.direction,
+          color: Theme.of(context).colorScheme.primary,
+          child: child,
+        );
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return GlowingOverscrollIndicator(
+          showLeading: false,
+          showTrailing: false,
+          axisDirection: details.direction,
+          color: Theme.of(context).colorScheme.primary,
+          child: child,
+        );
+    }
   }
 }

8ac8dd5683125a97b6366ac4d55c5a3d474f8c3b
canyonwan <canyonwan@126.com>
2023-08-24 15:02:08 +0800

    feat: 新增util

---
+++ lib/app/utils/index.dart
@@ -0,0 +1,25 @@
+library utils;
+
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+// import 'package:image_picker/image_picker.dart';
+import 'package:logger/logger.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:vms_client_flutter/app/widgets/index.dart';
+import 'package:vms_client_flutter/store/index.dart';
+
+part 'assets_picker.dart';
+part 'console.dart';
+part 'constants.dart';
+part 'convert.dart';
+part 'date.dart';
+part 'input_formatter.dart';
+part 'permission.dart';
+part 'screen.dart';
+part 'validator.dart';

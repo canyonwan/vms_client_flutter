56a1a2984b1834b0de3e39994d405612f4e2e1cd
canyonwan <canyonwan@126.com>
2023-08-24 15:03:47 +0800

    feat: init store

---
+++ lib/store/index.dart
@@ -0,0 +1,13 @@
+library store;
+
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vms_client_flutter/app/model/profile_model.dart';
+import 'package:vms_client_flutter/app/utils/index.dart';
+import 'package:vms_client_flutter/services/index.dart';
+import 'package:vms_client_flutter/theme.dart';
+
+part 'config.dart';
+part 'user.dart';

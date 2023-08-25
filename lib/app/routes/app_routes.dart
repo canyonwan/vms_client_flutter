40af3c0030007f2a2e9b4135ffcf953b405f287d
canyonwan <canyonwan@126.com>
2023-08-25 23:22:58 +0800

    feat: 新增路由

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -9,6 +9,9 @@ abstract class Routes {
   static const CART = _Paths.CART;
   static const MINE = _Paths.MINE;
   static const CIRCLE = _Paths.CIRCLE;
+  static const LOGIN = _Paths.LOGIN;
+  static const RESET_PASSWORD = _Paths.RESET_PASSWORD;
+  static const GOODS_DETAIL = _Paths.GOODS_DETAIL;
 }
 
 abstract class _Paths {
@@ -19,4 +22,7 @@ abstract class _Paths {
   static const CART = '/cart';
   static const MINE = '/mine';
   static const CIRCLE = '/circle';
+  static const LOGIN = '/login';
+  static const RESET_PASSWORD = '/reset-password';
+  static const GOODS_DETAIL = '/goods-detail';
 }

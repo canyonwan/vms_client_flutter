43c267af6ff72265b35457a2f766ccc777471872
canyonwan <canyonwan@126.com>
2023-08-25 23:22:54 +0800

    feat: 新增路由

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -6,10 +6,15 @@ import '../modules/category/bindings/category_binding.dart';
 import '../modules/category/views/category_view.dart';
 import '../modules/circle/bindings/circle_binding.dart';
 import '../modules/circle/views/circle_view.dart';
+import '../modules/goodsDetail/bindings/goods_detail_binding.dart';
+import '../modules/goodsDetail/views/goods_detail_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/login/index.dart';
 import '../modules/mine/bindings/mine_binding.dart';
 import '../modules/mine/views/mine_view.dart';
+import '../modules/resetPassword/bindings/reset_password_binding.dart';
+import '../modules/resetPassword/views/reset_password_view.dart';
 import '../modules/root/bindings/root_binding.dart';
 import '../modules/root/views/root_view.dart';
 
@@ -50,6 +55,28 @@ class AppPages {
       name: _Paths.CIRCLE,
       page: () => const CircleView(),
       binding: CircleBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+      children: [
+        GetPage(
+          name: _Paths.LOGIN,
+          page: () => const LoginView(),
+          binding: LoginBinding(),
+        ),
+      ],
+    ),
+    GetPage(
+      name: _Paths.RESET_PASSWORD,
+      page: () => const ResetPasswordView(),
+      binding: ResetPasswordBinding(),
+    ),
+    GetPage(
+      name: _Paths.GOODS_DETAIL,
+      page: () => const GoodsDetailView(),
+      binding: GoodsDetailBinding(),
     ),
   ];
 }

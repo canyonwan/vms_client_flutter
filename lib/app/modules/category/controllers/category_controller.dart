9fa1307c60df2dbffc17a3b94166d3f1188d43da
canyonwan <canyonwan@126.com>
2023-08-25 23:23:51 +0800

    feat: 新增分类页

--- lib/app/modules/category/controllers/category_controller.dart
+++ lib/app/modules/category/controllers/category_controller.dart
@@ -1,9 +1,26 @@
+import 'package:easy_refresh/easy_refresh.dart';
 import 'package:get/get.dart';
+import 'package:vms_client_flutter/app/api/index.dart';
+import 'package:vms_client_flutter/app/model/goods_category_model.dart';
+import 'package:vms_client_flutter/app/model/goods_page_model.dart';
 
 class CategoryController extends GetxController {
-  //TODO: Implement CategoryController
+  EasyRefreshController refreshController = EasyRefreshController(
+    controlFinishLoad: true,
+    controlFinishRefresh: true,
+  );
+  List<GoodsCategoryItemModel> list = [];
+  List<GoodsCategoryItemModel> subCateList = [];
+  int currentCateId = 0;
+  int currentSubCateId = 0;
 
-  final count = 0.obs;
+  int page = 1;
+  int pageSize = 10;
+  int total = 0;
+  List<GoodsItemModel> goodsList = [];
+
+  bool loadingData = false;
+
   @override
   void onInit() {
     super.onInit();
@@ -12,12 +29,56 @@ class CategoryController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+    getGoodsCategoryTree();
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  // 获取商品分类树
+  Future<void> getGoodsCategoryTree() async {
+    var r = await queryGoodsCategoryTree();
+    if (r.list.isNotEmpty) {
+      currentCateId = r.list.first.id;
+      getGoodsPage();
+      if (r.list.first.children != null && r.list.first.children!.isNotEmpty) {
+        currentSubCateId = r.list.first.children!.first.id;
+        subCateList = r.list.first.children!;
+      }
+      list = r.list;
+    }
+    update();
   }
 
-  void increment() => count.value++;
+  // 获取商品分页
+  Future<void> getGoodsPage() async {
+    var res = await queryGoodsPage(
+        page: page,
+        size: pageSize,
+        categoryIds: '$currentCateId,$currentSubCateId');
+    total = res.total;
+    if (res.content != null && res.content!.isNotEmpty) {
+      goodsList.addAll(res.content!);
+    }
+    loadingData = false;
+    update();
+  }
+
+  Future<void> onLoadMore() async {
+    if (goodsList.length == total) {
+      refreshController.finishLoad(IndicatorResult.noMore);
+      return;
+    }
+    page++;
+    await getGoodsPage();
+    refreshController.finishLoad(IndicatorResult.success);
+  }
+
+  void onChangeCate(GoodsCategoryItemModel item) {
+    goodsList.clear();
+    currentCateId = item.id;
+    currentSubCateId = 0;
+    subCateList = item.children ?? [];
+    page = 1;
+    loadingData = true;
+    getGoodsPage();
+    update();
+  }
 }

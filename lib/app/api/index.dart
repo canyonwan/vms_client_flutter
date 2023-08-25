f1ff447bf5f117743961db2557d1fb26a8d958d8
canyonwan <canyonwan@126.com>
2023-08-25 23:25:19 +0800

    feat: 新增接口

---
+++ lib/app/api/index.dart
@@ -0,0 +1,13 @@
+library api;
+
+import 'package:vms_client_flutter/app/model/goods_category_model.dart';
+import 'package:vms_client_flutter/app/model/goods_detail_model.dart';
+import 'package:vms_client_flutter/app/model/goods_page_model.dart';
+import 'package:vms_client_flutter/app/model/home_banner_model.dart';
+import 'package:vms_client_flutter/services/index.dart';
+
+part 'cart.dart';
+part 'common.dart';
+part 'goods.dart';
+part 'order.dart';
+part 'user.dart';

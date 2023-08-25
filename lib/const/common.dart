f17b3112e0229bcce28bb7b935b9b8fb63468f5d
canyonwan <canyonwan@126.com>
2023-08-25 23:24:07 +0800

    feat: 新增首页接口

--- lib/const/common.dart
+++ lib/const/common.dart
@@ -1,5 +1,3 @@
-const double kBorderRadius = 6;
-const double kPagePadding = 10;
-
 const String kProdApiUrl = 'https://api.canyonwan.top/v1/frontend/';
+const String kImgUrl = 'https://file.canyonwan.top/';
 const String kDevApiUrl = 'http://127.0.0.1:8080/v1/frontend/';
